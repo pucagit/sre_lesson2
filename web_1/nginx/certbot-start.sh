295ee9656866d88702c6b0f8981298b35d3ef4d6
Phuc <hanoi2003.a@gmail.com>
2025-09-11 21:21:51 +0700

    try merging cerbot and nginx inside a container

---
+++ web_1/nginx/certbot-start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+DOMAIN="web1.pucavv.io.vn"
+EMAIL="admin@pucavv.io.vn"
+
+certbot certificates >/dev/null 2>&1 || true
+
+if ! certbot certificates | grep -q "Domains: $DOMAIN"; then
+    certbot --nginx -d "$DOMAIN" --redirect --agree-tos --no-eff-email --email "$EMAIL" || true
+fi
+
+(
+    while true; do
+        sleep 12h
+        certbot renew --nginx --quiet --deploy-hook "nginx -s reload" | true
+    done
+)
