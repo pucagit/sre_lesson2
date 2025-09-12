ecfc75ac14c43316c24ff710a6baacabce6345af
Phuc <hanoi2003.a@gmail.com>
2025-09-12 09:46:10 +0700

    run web container as non-root user

--- web_1/nginx/certbot-start.sh
+++ web_1/nginx/certbot-start.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
-set -eu
 
 DOMAIN="web2.pucavv.io.vn"
 EMAIL="admin@pucavv.io.vn"
-
-certbot certificates >/dev/null 2>&1 || true
 
 if ! certbot certificates | grep -q "Domains: $DOMAIN"; then
     echo "Obtaining new certificate for $DOMAIN"
     certbot --nginx -d "$DOMAIN" --redirect --agree-tos --no-eff-email --email "$EMAIL" --force-renewal || true
 fi
-
-(
-    while true; do
-        sleep 12h
-        certbot renew --nginx --quiet --deploy-hook "nginx -s reload" | true
-    done
-) &
 
 exec nginx -g 'daemon off;'
